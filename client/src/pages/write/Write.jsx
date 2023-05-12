23398acf7e729782a628aa3e23e7942f8be407e4
Harsh <hash@gmail.com>
2023-05-12 23:13:48 +0530

    Added contact to admin with email

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Write.css";
+import { useDispatch, useSelector } from "react-redux";
+import emailjs from "emailjs-com";
 export default function Write() {
+  const user = useSelector((state) => state.checkUser.user);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    emailjs
+      .sendForm(
+        "gymnationbokaro",
+        "template_xnkc94r",
+        e.target,
+        "VBVZ4LukYG0GY6LK_"
+      )
+      .then(
+        (result) => {
+          alert(
+            "Email sent successfully to the admin. We will contact you asap."
+          );
+        },
+        (error) => {
+          console.log("Something went wronge", "Error: ", error.message);
+        }
+      );
+  };
   return (
     <div className="write">
-        <img src="./assets/GymLogo.png" alt="" className="write-image"/>
-      <form className="write-form">
+      <img src="./assets/GymLogo.png" alt="" className="write-image" />
+      <form className="write-form" onSubmit={sendEmail}>
         <div className="write-form-group">
           <label htmlFor="file-input">
             <i className="write-icon fa-solid fa-plus"></i>
@@ -15,6 +39,17 @@ export default function Write() {
             id="text-input"
             placeholder="Title"
             className="write-input"
+            name="TITLE"
+            autoFocus={true}
+          />
+          <input
+            style={{ display: "none" }}
+            type="text"
+            id="text-input"
+            placeholder="Title"
+            className="write-input"
+            value={user.username}
+            name="SENDER"
             autoFocus={true}
           />
         </div>
@@ -22,10 +57,13 @@ export default function Write() {
           <textarea
             placeholder="Write to us here..."
             type="text"
+            name="MESSAGE"
             className="write-input write-text"
           ></textarea>
         </div>
-        <button className="write-submit">Submit</button>
+        <button className="write-submit" type="submit">
+          Send mail <i className="fa-solid fa-paper-plane-top"></i>
+        </button>
       </form>
     </div>
   );
